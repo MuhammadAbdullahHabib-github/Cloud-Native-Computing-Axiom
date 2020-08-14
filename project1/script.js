261b462c19d1fa48b22b780922fb845ee872f5d3
Muhammad Abdullah Habib <u2019274@giki.edu.pk>
2020-08-15 04:40:00 +0500

    Update script.js

--- project1/script.js
+++ project1/script.js
@@ -25,15 +25,19 @@ function showSuccess(input) {
 
 // Function to check Emaill(
 
-function isValidEmail(email) {
+function isValidEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input , `${getRealId(input)} is invalid`);
+    }
 }
 
 // Function which convert alphabets
 
 function getRealId(input) {
-   data = input.id.charAt(0).toUpperCase() + input.id.slice(1 ).toLowerCase();
+   data = input.id.charAt(0).toUpperCase() + input.id.slice(1,8).toLowerCase();
    return data;
 }
 
@@ -49,11 +53,23 @@ function checkRequired(inputArray) {
     });
 }
 
+//Function to check lenght
 
+function checkLength(input,min,max) {
+    if(input.value.length < min ) {
+     showError(input,`${getRealId(input)} needs to be atleast ${min}`)
+    } else if(input.value.length > max) {
+     showError(input,`${getRealId(input)} needs to be atmost ${max}`)
+    } else {
+     showSuccess(input);
+    }
+}
 
 // event listner creates for Object
 form.addEventListener("submit",function(e) {
     e.preventDefault();
       checkRequired([username,email,password,password2]);
-
+      checkLength(username,3,10);
+      checkLength(password,6,30);
+      isValidEmail(email);
 })
