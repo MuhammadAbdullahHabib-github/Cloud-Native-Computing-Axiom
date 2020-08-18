6469804466e7d1a4c9509ac8a30e0c4e3e819fd2
Muhammad Abdullah Habib <u2019274@giki.edu.pk>
2020-08-19 04:59:03 +0500

    final version of project 2

--- project2/script.js
+++ project2/script.js
@@ -4,24 +4,21 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 const screen = document.querySelector('.screen');
+let option = document.querySelectorAll('.movieClass');
 let movietrailer = ['https://www.youtube.com/embed/qSqVVswa420','https://www.youtube.com/embed/qSqVVswa420','https://www.youtube.com/embed/7TavVZMewpY','https://www.youtube.com/embed/XiHiW4N7-bo','https://www.youtube.com/embed/2QKg5SZ_35I']
 let ticketPrice = +movieSelect.value;
 
 function updateSlectedCount(){
         let selectedSeats = document.getElementsByClassName('chose')
         let totalSeats = selectedSeats.length;
-        console.log(totalSeats);
         count.innerText = totalSeats;
         total.innerText = movieSelect.value * totalSeats;
 }
 
 movieSelect.addEventListener('change', (e) =>{
         ticketPrice = +e.target.value;
-        console.log(e.target.id)
-        screen.innerHTML=`<iframe class='movie-trailer' width="650" height="290" src="${ movietrailer[+e.target.className]} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
-        
-       
-        updateSlectedCount();
+        screen.innerHTML = `<iframe class='movie-trailer' width="650" height="290" src="${movietrailer[+e.target.value]}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+          updateSlectedCount();
 })
 
 // Event Listner on avalible seats
@@ -30,6 +27,11 @@ container.addEventListener('click', function(e) {
                 e.target.src='artboard-manImg.png'; 
                 e.target.className='chose'
                 updateSlectedCount();
-      }})
+ }})
+
+ 
+
+
+
 
  
