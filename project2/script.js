63e2880b3654cbabf02832729eaed0aae89508c3
Muhammad Abdullah Habib <u2019274@giki.edu.pk>
2020-08-19 02:03:29 +0500

    Images are added in file

--- project2/script.js
+++ project2/script.js
@@ -5,9 +5,18 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 const ticketPrice = +movieSelect.value;
 
+function updateSlectedCount(){
+        const selectedSeats = document.querySelectorAll('chose')
+        const totalSeats = selectedSeats.length ;
+        console.log(totalSeats);
+}
+
 // Event Listner on avalible seats
 container.addEventListener('click', function(e) {
         if(e.target.classList.contains('vacentSeat')) {
-                e.target.src='../project2\Images\artboard-manImg.png';
-        }
-})
+                if(e.target.src='artboard-vacantImg.png'){
+                e.target.src='artboard-manImg.png'; 
+                e.target.class='chose'
+                }
+                updateSlectedCount();
+      }})
