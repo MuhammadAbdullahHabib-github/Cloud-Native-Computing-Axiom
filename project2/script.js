fda11cca19721e36b44b96abac3be232da947ffd
Muhammad Abdullah Habib <u2019274@giki.edu.pk>
2020-08-20 17:32:38 +0500

    1 issue resolved

--- project2/script.js
+++ project2/script.js
@@ -1,6 +1,30 @@
+const movieOption = document.querySelector('#movie');
+let movieTrailor = ['https://www.youtube.com/embed/qSqVVswa420','https://www.youtube.com/embed/BIhNsAtPbPI','https://www.youtube.com/embed/7TavVZMewpY','https://www.youtube.com/embed/XiHiW4N7-bo','https://www.youtube.com/embed/2QKg5SZ_35I']
+const screen = document.querySelector('.screen');
+movieOption.addEventListener("change", (e) => {
+        if(+e.target.value === 200){
+                screen.innerHTML = `<iframe class='movie-trailer' width='650' height='290' src='${movieTrailor[0]}' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>`;
+        }else if(+e.target.value === 150){
+                screen.innerHTML = `<iframe class='movie-trailer' width='650' height='290' src='${movieTrailor[1]}' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>`;
+        }else if(+e.target.value === 210){
+                screen.innerHTML = `<iframe class='movie-trailer' width='650' height='290' src='${movieTrailor[2]}' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>`;
+        }else if(+e.target.value === 180){
+                screen.innerHTML = `<iframe class='movie-trailer' width='650' height='290' src='${movieTrailor[3]}' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>`;
+        }else if(+e.target.value === 140){
+                screen.innerHTML = `<iframe class='movie-trailer' width='650' height='290' src='${movieTrailor[4]}' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>`;
+        }
+})
 
+function updateCount(){
+        
+}
 
-
+let container = document.querySelector('.container');
+container.addEventListener("click",(e) => {
+        if(e.target.classList.contains('vacentSeat')){
+                e.target.src = 'occupiedImg2.png';
+        }
+})
 
 
 
